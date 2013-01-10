d59a4c0d06df23c8fd3af1e5ec3c1eabc70d34eb
Dave Drager <ddrager@gmail.com>
2013-01-10 10:57:47 -0500

    Added url checking to ensure that the request is coming from the same host. This ensures that you can't check other sites with your own site's script.

--- src/service/index.php
+++ src/service/index.php
@@ -1,15 +1,20 @@
 <?php
-require_once('SocialCount.php');
+if (preg_match('/^http(s?):\/\/'.$_SERVER['HTTP_HOST'].'(:\d+)?\//',$_GET['url'])) {
 
-try {
-	$social = new SocialCount($_GET['url']);
+        require_once('SocialCount.php');
 
-	$social->addNetwork(new Twitter());
-	$social->addNetwork(new Facebook());
-	$social->addNetwork(new GooglePlus());
-	// $social->addNetwork(new ShareThis());
+        try {
+                $social = new SocialCount($_GET['url']);
 
-	echo $social->toJSON();
-} catch(Exception $e) {
-	echo '{"error": "' . $e->getMessage() . '"}';
+                $social->addNetwork(new Twitter());
+                $social->addNetwork(new Facebook());
+                $social->addNetwork(new GooglePlus());
+                // $social->addNetwork(new ShareThis());
+
+                echo $social->toJSON();
+        } catch(Exception $e) {
+                echo '{"error": "' . $e->getMessage() . '"}';
+        }
+} else {
+        echo '{"error": "URL not authorized"}';
 }
