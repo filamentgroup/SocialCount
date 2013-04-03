4134f79046877bdacc862fe628073f5b848ff309
Andrew Volozhanin <linuxheadrus@gmail.com>
2013-04-04 01:34:01 +0600

    removed unused code (including all counts code), replaced featureTest to modernizr

--- src/socialcount.js
+++ src/socialcount.js
@@ -2,66 +2,20 @@
 ;(function( win, doc, $ ) {
 
 	var $loadingIndicator,
-		$count;
-
-	function featureTest( prop, unprefixedProp ) {
-		var style = doc.createElement('social').style,
-			prefixes = 'webkit Moz o ms'.split(' ');
-
-		if( unprefixedProp in style ) {
-			return true;
-		}
-		for( var j = 0, k = prefixes.length; j < k; j++ ) {
-			if( ( prefixes[ j ] + prop ) in style ) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	function removeFileName( src ) {
-		var split = src.split( '/' );
-		split.pop();
-		return split.join( '/' ) + '/';
-	}
-
-	function resolveServiceDir() {
-		var baseUrl;
-
-		$( 'script' ).each(function() {
-			var src = this.src || '',
-				dir;
-			if( src.match( SocialCount.scriptSrcRegex ) ) {
-				baseUrl = removeFileName( src );
-				return false;
-			}
-		});
-
-		return baseUrl;
-	}
+			Modernizr = window.Modernizr;
 
 	var SocialCount = {
 		// For A-grade experience, require querySelector (IE8+) and not BlackBerry or touchscreen
 		isGradeA: 'querySelectorAll' in doc && !win.blackberry && !('ontouchstart' in window) && !('onmsgesturechange' in window),
-		minCount: 1,
-		serviceUrl: 'service/index.php',
 		initSelector: '.socialcount',
 		classes: {
 			gradeA: 'grade-a',
 			active: 'active',
 			touch: 'touch',
 			hover: 'hover',
-			noTransforms: 'no-transforms',
-			showCounts: 'counts',
-			countContent: 'count',
-			minCount: 'minimum',
 			activateOnHover: 'activate-on-hover',
 			activateOnClick: 'activate-on-click',
-			vkMini: ''
 		},
-		thousandCharacter: 'K',
-		millionCharacter: 'M',
-		missingResultText: '-',
 		activateOnClick: false, // default is hover
 		selectors: {
 			facebook: '.facebook',
@@ -74,39 +28,13 @@
 		googleplusTooltip: 'table.gc-bubbleDefault',
 		vkElementID: 'vk_like',
 		vkType: "button", 
-		scriptSrcRegex: /socialcount[\w.]*.js/i,
-		plugins: {
-			init: [],
-			bind: []
-		},
 
-		// private, but for testing
-		cache: {},
-
-		removeFileName: removeFileName,
-		resolveServiceDir: resolveServiceDir,
-
-		isCssAnimations: function() {
-			return featureTest( 'AnimationName', 'animationName' );
-		},
-		isCssTransforms: function() {
-			return featureTest( 'Transform', 'transform' );
-		},
 		getUrl: function( $el ) {
 			return $el.attr('data-url') || location.href;
 		},
 		// Currently only available on Twitter
 		getShareText: function( $el ) {
 			return $el.attr('data-share-text' ) || '';
-		},
-		getFacebookAction: function( $el ) {
-			return ( $el.attr('data-facebook-action' ) || 'like' ).toLowerCase();
-		},
-		isCountsEnabled: function( $el ) {
-			return $el.attr('data-counts') === 'true';
-		},
-		isSmallSize: function( $el ) {
-			return $el.is( '.socialcount-small' );
 		},
 		getVKOptions: function($el) {
 			return {
@@ -115,67 +43,13 @@
 				vkType: $el.attr('data-vk-type') || SocialCount.vkType
 			};
 		},
-		getCounts: function( $el, url ) {
-			var map = SocialCount.selectors,
-				cache = SocialCount.cache,
-				counts = {},
-				$networkNode,
-				$countNode,
-				j;
-
-			for( j in map ) {
-				$networkNode = $el.find( map[ j ] );
-				$countNode = $networkNode.find( '.' + SocialCount.classes.countContent );
-
-				if( $countNode.length ) {
-					counts[ j ] = $countNode;
-				} else {
-					counts[ j ] = $count.clone();
-					$networkNode.append( counts[ j ] );
-				}
-			}
-
-			if( !cache[ url ] ) {
-				cache[ url ] = $.ajax({
-					url: resolveServiceDir() + SocialCount.serviceUrl,
-					data: {
-						url: url
-					},
-					dataType: 'json'
-				});
-			}
-
-			cache[ url ].done( function complete( data ) {
-				for( var j in data ) {
-					if( data.hasOwnProperty( j ) ) {
-						if( counts[ j ] && data[ j ] > SocialCount.minCount ) {
-							counts[ j ].addClass( SocialCount.classes.minCount )
-								.html( SocialCount.normalizeCount( data[ j ] ) );
-						}
-					}
-				}
-			});
-
-			return cache[ url ];
-		},
 		init: function( $el ) {
-			var options = {
-					facebookAction: SocialCount.getFacebookAction( $el ),
-				},
-				classes = [ options.facebookAction ],
-				isSmall = SocialCount.isSmallSize( $el ),
-				url = SocialCount.getUrl( $el ),
-				initPlugins = SocialCount.plugins.init,
-				countsEnabled = SocialCount.isCountsEnabled( $el );
+			var options = {},
+					classes = [],
+					url = SocialCount.getUrl( $el );
 
 			if( SocialCount.isGradeA ) {
 				classes.push( SocialCount.classes.gradeA );
-			}
-			if( !SocialCount.isCssTransforms() ) {
-				classes.push( SocialCount.classes.noTransforms );
-			}
-			if( countsEnabled ) {
-				classes.push( SocialCount.classes.showCounts );
 			}
 			if( SocialCount.activateOnClick ) {
 				classes.push( SocialCount.classes.activateOnClick );
@@ -192,36 +66,11 @@
 			
 			$el.addClass( classes.join(' ') );
 
-			for( var j = 0, k = initPlugins.length; j < k; j++ ) {
-				initPlugins[ j ].call( $el );
-			}
-
 			if( SocialCount.isGradeA ) {
-				SocialCount.bindEvents( $el, url, options, isSmall );
-			}
-
-			if( countsEnabled && !isSmall ) {
-				SocialCount.getCounts( $el, url );
+				SocialCount.bindEvents( $el, url, options );
 			}
 		},
-		normalizeCount: function( count ) {
-			if( !count && count !== 0 ) {
-				return SocialCount.missingResultText;
-			}
-			// > 1M
-			if( count >= 1000000 ) {
-				return Math.floor( count / 1000000 ) + SocialCount.millionCharacter;
-			}
-			// > 100K
-			if( count >= 100000 ) {
-				return Math.floor( count / 1000 ) + SocialCount.thousandCharacter;
-			}
-			if( count > 1000 ) {
-				return ( count / 1000 ).toFixed( 1 ).replace( /\.0/, '' ) + SocialCount.thousandCharacter;
-			}
-			return count;
-		},
-		bindEvents: function( $el, url, options, isSmall ) {
+		bindEvents: function( $el, url, options ) {
 			function bind( $a, html, jsUrl, jsInline ) {
 				// IE bug (tested up to version 9) with :hover rules and iframes.
 				var isTooltipActive = false,
@@ -312,46 +161,37 @@
 				});
 			} // end bind()
 
-			if( !isSmall ) {
-				var shareText = SocialCount.getShareText( $el );
+			var shareText = SocialCount.getShareText( $el );
 
-				bind( $el.find( SocialCount.selectors.facebook + ' a' ),
-					'<iframe src="//www.facebook.com/plugins/like.php?href=' + encodeURI( url ) +
-						( SocialCount.locale ? '&locale=' + SocialCount.locale : '' ) +
-						'&amp;send=false&amp;layout=button_count&amp;width=100&amp;show_faces=true&amp;action=' + options.facebookAction +
-						'&amp;colorscheme=light&amp;font=arial&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden;" allowTransparency="true"></iframe>' );
+			bind( $el.find( SocialCount.selectors.facebook + ' a' ),
+				'<iframe src="//www.facebook.com/plugins/like.php?href=' + encodeURI( url ) +
+					( SocialCount.locale ? '&locale=' + SocialCount.locale : '' ) +
+					'&send=false&layout=button_count&width=100&show_faces=true&action=like&colorscheme=light&font=arial&height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden;" allowTransparency="true"></iframe>' );
 
-				bind( $el.find( SocialCount.selectors.twitter + ' a' ),
-					'<a href="https://twitter.com/share" class="twitter-share-button"' +
-						' data-url="' + encodeURI( url ) + '"' +
-						( shareText ? ' data-text="' + shareText + '"': '' ) +
-						' data-count="none" data-dnt="true">Tweet</a>',
-					'//platform.twitter.com/widgets.js' );
+			bind( $el.find( SocialCount.selectors.twitter + ' a' ),
+				'<a href="https://twitter.com/share" class="twitter-share-button"' +
+					' data-url="' + encodeURI( url ) + '"' +
+					( shareText ? ' data-text="' + shareText + '"': '' ) +
+					' data-count="none" data-dnt="true">Tweet</a>',
+				'//platform.twitter.com/widgets.js' );
 
-				bind( $el.find( SocialCount.selectors.googleplus + ' a' ),
-					'<div class="g-plusone" data-size="medium" data-annotation="none"></div>',
-					'//apis.google.com/js/plusone.js' );		
+			bind( $el.find( SocialCount.selectors.googleplus + ' a' ),
+				'<div class="g-plusone" data-size="medium" data-annotation="none"></div>',
+				'//apis.google.com/js/plusone.js' );		
 
-				bind( $el.find( SocialCount.selectors.odnoklassniki + ' a' ),
-					'<a target="_blank" class="mrc__plugin_uber_like_button" href="http://connect.mail.ru/share" data-mrc-config="{\'cm\' : \'1\', \'ck\' : \'1\', \'sz\' : \'20\', \'st\' : \'2\', \'tp\' : \'ok\', \'width\' : \'91%\'}">Нравится</a>',
-					'//cdn.connect.mail.ru/js/loader.js' );		
+			bind( $el.find( SocialCount.selectors.odnoklassniki + ' a' ),
+				'<a target="_blank" class="mrc__plugin_uber_like_button" href="http://connect.mail.ru/share" data-mrc-config="{\'cm\' : \'1\', \'ck\' : \'1\', \'sz\' : \'20\', \'st\' : \'2\', \'tp\' : \'ok\', \'width\' : \'91%\'}">Нравится</a>',
+				'//cdn.connect.mail.ru/js/loader.js' );		
 
-				if ( options.vk ) {
-					bind( $el.find( SocialCount.selectors.vk + ' a' ),
-						'<div id="' + options.vk.vkElementID + '"></div>',
-						'//vk.com/js/api/openapi.js',
-						function(){
-							if (!VK._apiId) //Init only the first time
-								VK.init({apiId: options.vk.vkApiID, onlyWidgets: true});
-							VK.Widgets.Like(options.vk.vkElementID, {type: options.vk.vkType});
-						});
-				}
-			}
-
-			// Bind events on other non-stock widgets, like sharethis
-			var bindPlugins = SocialCount.plugins.bind;
-			for( var j = 0, k = bindPlugins.length; j < k; j++ ) {
-				bindPlugins[ j ].call( $el, bind, url, isSmall );
+			if ( options.vk ) {
+				bind( $el.find( SocialCount.selectors.vk + ' a' ),
+					'<div id="' + options.vk.vkElementID + '"></div>',
+					'//vk.com/js/api/openapi.js',
+					function(){
+						if (!VK._apiId) //Init only the first time
+							VK.init({apiId: options.vk.vkApiID, onlyWidgets: true});
+						VK.Widgets.Like(options.vk.vkElementID, {type: options.vk.vkType});
+					});
 			}
 		} // end bindEvents()
 	};
@@ -360,11 +200,7 @@
 		// Thanks to http://codepen.io/ericmatthys/pen/FfcEL
 		$loadingIndicator = $('<div>')
 			.addClass('loading')
-			.html( SocialCount.isCssAnimations() ? new Array(4).join('<div class="dot"></div>') : 'Loading' );
-
-		$count = $('<span>')
-			.addClass( SocialCount.classes.countContent )
-			.html('&#160;');
+			.html( Modernizr.cssanimations ? new Array(4).join('<div class="dot"></div>') : 'Loading' );
 
 		$( SocialCount.initSelector ).each(function() {
 			var $el = $(this);
