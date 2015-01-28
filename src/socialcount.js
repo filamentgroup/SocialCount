3de539a28268cc7e45f1d672829b04dc56b932e0
Zach Leatherman <zachleatherman@gmail.com>
2015-01-28 13:04:02 -0600

    Fixes #30

--- src/socialcount.js
+++ src/socialcount.js
@@ -313,7 +313,7 @@
 					});
 
 				bind( $el.find( SocialCount.selectors.googleplus + ' a' ),
-					'<div class="g-plusone" data-size="medium" data-annotation="none"></div>',
+					'<div class="g-plusone" data-size="medium" data-annotation="none" data-href="' + url + '"></div>',
 					'//apis.google.com/js/plusone.js',
 					function( el ) {
 						gapi.plusone.go( el );
