76eb27c8abf8aa8164ad928181d5186d0ec0d295
Zach Leatherman <zachleatherman@gmail.com>
2016-08-19 17:05:29 -0500

    Fixes #93

--- README.md
+++ README.md
@@ -14,14 +14,13 @@ SocialCount is a small jQuery plugin for progressively enhanced, lazy loaded, mo
 
  * Currently supports Facebook, Twitter, and Google Plus.
  * **1.12 kB** CSS and **2.73 kB** JS (after Min+GZip—compared to 366 kB, or 100 kB repeat view, for Facebook, Twitter, and Google Plus widgets)
- * 1 JS request, 1 CSS request, 1 optional request for icons, 1 optional AJAX request for counts (compared to 21 total requests for Facebook, Twitter, and Google Plus widgets)
+ * 1 JS request, 1 CSS request, 1 optional request for icons (compared to 21 total requests for Facebook, Twitter, and Google Plus widgets)
  * Easy to fit in with existing page design
  * Ability to share without JavaScript or before JavaScript has loaded.
  * Works with mouse, touchscreen, or keyboard.
 	 * Mouse: On hover, loads a social network's native widget so that the user can Like/Recommend/+1 without leaving the current page.
 	 * Touch screen: simple redirects to dedicated network share pages.
 	 * Keyboard: Concise tab order.
- * Option to conditionally display count if it's above a minimum threshold. Avoid goose egg ghost-town.
  * Intelligent client-side caching so that two or more widgets with the same share URL only make one AJAX request.
  * Requires jQuery 1.6+
 
@@ -31,13 +30,11 @@ SocialCount is a small jQuery plugin for progressively enhanced, lazy loaded, mo
 
 ## Getting Started
 
-Install via Bower (`bower install --save socialcount`) or via [NPM](https://www.npmjs.com/package/fg-socialcount) (`npm install --save fg-socialcount`).
+Install via [NPM](https://www.npmjs.com/package/fg-socialcount) (`npm install --save fg-socialcount`).
 
 Alternatively, the following archive contains both minified (`socialcount.min.js`+`socialcount.min.css`) and unminified (`socialcount.js`+`socialcount.css`) versions of the JS and CSS required to use SocialCount.
 
 By default, the social networking icons are not included. However, SocialCount publishes a version that does include the social networking icons: Use the regular `socialcount.js` with `socialcount-with-icons.css` or `socialcount-with-icons.min.css` and the included SD and HD image sprites.
-
-The archive also contains the **optional** PHP service files for the AJAX request to retrieve the share counts (requires PHP 5 with curl). To enable this feature for your widget, use `<ul class="socialcount" data-counts="true">`.
 
 ### Download [socialcount.zip][zipfile]
 
@@ -84,9 +81,6 @@ Touchscreens (Links Only)
 ## Contributing
 Add unit tests for any new or changed functionality. Lint and test your JavaScript code using [grunt](https://github.com/cowboy/grunt) and the `grunt qunit` command.
 
-To test the PHP code, navigate to the `service` directory and run `phpunit socialcount_service_test.php`
-
-
 ## Release History
 * `v0.1.0` Initial release
 * `v0.1.1` Fix for IE (tested up to 9) issue with iframes and :hover rules
@@ -99,6 +93,7 @@ To test the PHP code, navigate to the `service` directory and run `phpunit socia
 * `v0.1.8` Bug fix: #45
 * [`v0.1.9`](https://github.com/filamentgroup/SocialCount/releases/tag/v0.1.9)
 * [`v0.2.0`](https://github.com/filamentgroup/SocialCount/releases/tag/v0.2.0) Removes Twitter from service, per Twitter’s API retirement.
+* [`v1.0.0`](https://github.com/filamentgroup/SocialCount/releases/tag/v1.0.0) Removes counts from SocialCount.
 
 ## Advanced Options
 
--- test/socialcount_test.js
+++ test/socialcount_test.js
@@ -20,51 +20,6 @@
 			raises(block, [expected], [message])
 	*/
 
-	module('testNormalizeCount');
-
-	test( 'less than 1000', function() {
-		equal(SocialCount.normalizeCount(null), '-');
-		equal(SocialCount.normalizeCount(0), 0);
-		equal(SocialCount.normalizeCount(999), 999);
-		equal(SocialCount.normalizeCount(1001), '1K');
-		equal(SocialCount.normalizeCount(1001), '1K');
-		equal(SocialCount.normalizeCount(1900), '1.9K');
-		equal(SocialCount.normalizeCount(1949), '1.9K');
-		// a lot of browsers report 1.9K here (bad rounding)
-		//equal(SocialCount.normalizeCount(1950), '2K');
-		equal(SocialCount.normalizeCount(1999), '2K');
-		equal(SocialCount.normalizeCount(2000), '2K');
-		equal(SocialCount.normalizeCount(2049), '2K');
-		// a lot of browsers report 2K here (bad rounding)
-		//equal(SocialCount.normalizeCount(2050), '2.1K');
-	});
-
-	test( 'around 100,000', function() {
-		equal(SocialCount.normalizeCount(99499), '99.5K');
-		equal(SocialCount.normalizeCount(99501), '99.5K');
-		// if >= 100K, uses Math.floor
-		equal(SocialCount.normalizeCount(100000), '100K');
-		equal(SocialCount.normalizeCount(100999), '100K');
-		equal(SocialCount.normalizeCount(101000), '101K');
-		equal(SocialCount.normalizeCount(101999), '101K');
-	});
-
-	test( 'around one million', function() {
-		// uses Math.floor
-		equal(SocialCount.normalizeCount(999499), '999K');
-		equal(SocialCount.normalizeCount(999999), '999K');
-		equal(SocialCount.normalizeCount(1000000), '1M');
-	});
-
-	module( 'testServiceNormalization' );
-	test( 'filenameRemoval', function() {
-		// uses Math.floor
-		equal( SocialCount.removeFileName( 'http://localhost/SocialCount/src/socialcount.js' ), 'http://localhost/SocialCount/src/' );
-		equal( SocialCount.removeFileName( 'http://localhost/SocialCount/dist/socialcount.min.js' ), 'http://localhost/SocialCount/dist/' );
-		equal( SocialCount.removeFileName( 'http://fgte.st/SocialCount/src/socialcount.js' ), 'http://fgte.st/SocialCount/src/' );
-		equal( SocialCount.removeFileName( 'http://fgte.st/SocialCount/dist/socialcount.min.js' ), 'http://fgte.st/SocialCount/dist/' );
-	});
-
 	module('testInitialize', {
 		setup: function() {
 			var $fixture = $('#qunit-fixture'),
@@ -97,66 +52,6 @@
 		ok( SocialCount.isCssTransforms() ?
 			!$('#test').hasClass( SocialCount.classes.noTransforms ) :
 			$('#test').hasClass( SocialCount.classes.noTransforms ) );
-
-		ok( SocialCount.isCountsEnabled( $( '#test' ) ) ?
-			$('#test').hasClass( SocialCount.classes.showCounts ) :
-			!$('#test').hasClass( SocialCount.classes.showCounts ) );
-	});
-
-	module('testAjax', {
-		setup: function() {
-			var $fixture = $('#qunit-fixture');
-
-			$fixture.append( '<ul id="test" class="socialcount" data-url="http://www.google.com/"><li class="facebook"><a href="https://www.facebook.com/sharer/sharer.php?u=http://www.google.com/" title="Share on Facebook"><span class="icon icon-facebook"></span><span class="count">Like</span></a></li><li class="twitter"><a href="https://twitter.com/intent/tweet?text=http://www.google.com/" title="Share on Twitter"><span class="icon icon-twitter"></span><span class="count">Tweet</span></a></li><li class="googleplus"><a href="https://plus.google.com/share?url=http://www.google.com/" title="Share on Google Plus"><span class="icon icon-googleplus"></span><span class="count">+1</span></a></li></ul>' );
-		}
-	});
-
-	asyncTest( 'Test Mock Request to Service', 3, function() {
-		var dfd = $.Deferred(),
-			$test = $( '#test' ),
-			gplusLabel = $test.find( '.googleplus .count' ).html();
-
-		SocialCount.cache['http://www.google.com/'] = dfd.promise();
-
-		SocialCount.getCounts( $test, 'http://www.google.com/' ).done(function() {
-			strictEqual( $test.find( '.twitter .count' ).html(), '11.5M' );
-			strictEqual( $test.find( '.facebook .count' ).html(), '5.2M' );
-			strictEqual( $test.find( '.googleplus .count' ).html(), '1.6M' );
-
-			start();
-		});
-
-		window.setTimeout(function() {
-			dfd.resolve({
-				'twitter': 11464062,
-				'facebook': 5189703,
-				'googleplus': 1570539
-			});
-		}, 50 );
-	});
-
-	asyncTest( 'Test Mock Request to Service, Less than Min Count', 3, function() {
-		var dfd = $.Deferred(),
-			$test = $( '#test' ),
-			gplusLabel = $test.find( '.googleplus .count' ).html();
-
-		SocialCount.cache['http://www.google.com/'] = dfd.promise();
-
-		SocialCount.getCounts( $test, 'http://www.google.com/' ).done(function() {
-			strictEqual( $test.find( '.twitter .count' ).html(), '11.5M' );
-			strictEqual( $test.find( '.facebook .count' ).html(), '5.2M' );
-			strictEqual( $test.find( '.googleplus .count' ).html(), gplusLabel );
-
-			start();
-		});
-
-		window.setTimeout(function() {
-			dfd.resolve({
-				'twitter': 11464062,
-				'facebook': 5189703,
-				'googleplus': SocialCount.minCount - 1
-			});
-		}, 50 );
 	});
 
 	module('testInitializeNoUrl', {
